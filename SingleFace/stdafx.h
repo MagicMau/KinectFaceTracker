5f9e3453f1075d98021f46201f92ddd8ed20e533
Maurits Elbers <melbers@deloitte.nl>
2015-02-04 12:47:11 +0100

    original as downloaded from http://forums.bistudio.com/showthread.php?100530-Update-FaceTrackNoIR/page56

---
+++ SingleFace/stdafx.h
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="stdafx.h" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
+
+// Windows Header Files:
+#include <winsock2.h>
+#include <windows.h>
+#include <Shellapi.h>
+
+// C RunTime Header Files
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <crtdbg.h>
+
